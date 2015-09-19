54c4aee9e81e13774eff5a8922bf9c3048e2f424
FeynmanTech <astro.link.75@gmail.com>
2015-09-18 17:31:42 -0700

    Update reading.js

--- reading.js
+++ reading.js
@@ -4,24 +4,24 @@
     $mystr{12} = "!";
     print $mystr;
 ?>
-output: Hello, world!
+// output: Hello, world!
 
 $a = 1;
 if (isset($a)) print "A is set";
 if (isset($b)) print "B is set";
-output: A is set
+// output: A is set
 
 print(strlen("QWERTYUIOP"));
-output: 10
+// output: 10
 
 $str = "abcde";
 print(strpos($str, "c"));
-output: 3
+// output: 3
 
 function factorial($n)
 {
   if ($n == 1) { return $n; } else { return $n * factorial($n - 1); }
 }
 print(factorial(4));
-output: 24
+// output: 24
 
